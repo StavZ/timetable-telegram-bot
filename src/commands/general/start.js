cec966a5a2ded5e254ba8bd2f0ed7e8cff3cc933
StavZDev <randy10220@gmail.com>
2021-10-02 16:58:56 +0500

    Update start.js

--- src/commands/general/start.js
+++ src/commands/general/start.js
@@ -1,6 +1,8 @@
 import { Context } from 'telegraf';
 import Client from '../../structures/client/Client.js';
 import Command from '../../structures/client/Command.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 export default class StartCommand extends Command {
   /**
@@ -27,6 +29,7 @@ export default class StartCommand extends Command {
   async exec (ctx, args) {
     const user = await this.client.userManager.getUser(ctx.from.id);
     if (!user) await this.client.userManager.createUser(ctx.from.id);
-    ctx.replyWithMarkdown(`Привет!\n\nМоя главная функция - это *автоматическая рассылка расписания* студентам колледжа ППК им. Н.Г. Славянова.\`*\`\n\nЧтобы выбрать группу /selectgroup.\n\nОзнакомится со всеми командами /help.\nРазработчик: [VK](https://vk.com/stavzdev) | [TG](https://t.me/stavzdev)\n\n\`*\`_Автоматическая рассылка включается по-умолчанию, когда Вы выбираете свою группу.\nЧтобы её отключить - _/autoscheduler.`);
+    const version = require('../../../package.json').version;
+    ctx.replyWithMarkdown(`Привет!\n\nМоя главная функция - это *автоматическая рассылка расписания* студентам колледжа ППК им. Н.Г. Славянова.\`*\`\n\nВыбрать группу /selectgroup.\nПоддержка /support.\nСписок изменений \`v${version}\` /changelog.\nОзнакомится со всеми командами /help.\n\nРазработчик: [VK](https://vk.com/stavzdev) | [TG](https://t.me/stavzdev)\n\n\`*\`_Автоматическая рассылка включается по-умолчанию, когда Вы выбираете свою группу.\nЧтобы её отключить - _/autoscheduler.`, { disable_web_page_preview: true });
   }
 }
