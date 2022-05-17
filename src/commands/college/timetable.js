a23d72b54850a8b089891984b1de3e6bfa9e268f
stavzdev <randy10220@gmail.com>
2022-05-17 19:19:30 +0500

    Update timetable.js

--- src/commands/college/timetable.js
+++ src/commands/college/timetable.js
@@ -39,8 +39,12 @@ export default class TimetableC extends Command {
     });
 
     this.client.action('show-remote-works', async (ctxu) => {
+      // @ts-ignore
+      let date = ctxu.update.callback_query.message.reply_markup.inline_keyboard.at(-2).at(0).text.split(' ').at(-1).split('/');
+      date = `${date[0]}-${date[1]}-${date[2].slice(2, 4)}`;
       const cmd = this.client.commands.get('remoteworks');
-      const rw = await this.client.rw.getByDate(user.group, this.client.moment().format('DD-MM-YY'));
+      const rw = await this.client.rw.getByDate(user.group, date);
+      // @ts-ignore
       return cmd.showByDate(ctxu, rw, true);
     });
 
@@ -76,13 +80,13 @@ export default class TimetableC extends Command {
     const keyboard = this.generateKeyboard(id, timetables, 2, 'user-timetable', key ? key : userTimetable.date.regular);
     keyboard.push([{ text: 'Расписание другой группы', callback_data: 'group-timetable' }]);
     if (userTimetable.lessons.filter((l) => l.isRemote).length) {
-      keyboard.push([{ text: 'Показать дистанционные задания', callback_data: 'show-remote-works' }]);
+      keyboard.push([{ text: `Показать дистанционные задания ${timetable.date.regular}`, callback_data: 'show-remote-works' }]);
     }
     keyboard.push([{ text: 'Убрать кнопки', callback_data: 'cancel-timetable' }]);
 
     if (editId) {
       this.client.telegram
-        .editMessageText(ctx.chat.id, editId, editId, msg, { reply_markup: { inline_keyboard: keyboard, one_time_keyboard: true }, disable_web_page_preview: true, parse_mode: 'Markdown' })
+        .editMessageText(ctx.chat.id, editId, editId, msg, { reply_markup: { inline_keyboard: keyboard }, disable_web_page_preview: true, parse_mode: 'Markdown' })
         .catch(this.client.logger.error);
     } else {
       ctx.replyWithMarkdown(msg, { reply_markup: { inline_keyboard: keyboard, one_time_keyboard: true }, disable_web_page_preview: true }).catch(this.client.logger.error);
@@ -94,16 +98,18 @@ export default class TimetableC extends Command {
    */
   async selectGroup(ctx) {
     let groups = await this.client.timetable.getGroups();
+    // @ts-ignore
     const keyboard = this.generateKeyboard(ctx.update.callback_query.message.message_id, groups, 4, 'group-selection', null);
     keyboard.push([{ text: 'Назад', callback_data: 'back-to-user-timetable' }]);
 
     groups.forEach((g) => {
+      // @ts-ignore
       this.client.action(`group-selection-${ctx.update.callback_query.message.message_id}-${g}`, (ctx) => {
         return this.showGroupSchedule(ctx, g, null, ctx.update.callback_query.message.message_id);
       });
     });
 
-    return ctx.editMessageText('Выберите группу из списка ниже:', { reply_markup: { inline_keyboard: keyboard, one_time_keyboard: true } });
+    return ctx.editMessageText('Выберите группу из списка ниже:', { reply_markup: { inline_keyboard: keyboard } });
   }
 
   /**
@@ -135,21 +141,26 @@ export default class TimetableC extends Command {
 
   /**
    * @param {number} id
-   * @param {Timetable[]} items
+   * @param {any[]} items
    * @param {number} chunk
    * @param {string} selItem
    */
   generateKeyboard(id, items, chunk, kbtype = 'user-timetable', selItem) {
+    // @ts-ignore
     items = selItem ? items.map((i) => i.date.regular).removeItem(selItem) : kbtype.includes('group') ? items : items.map((i) => i.date.regular);
     const keyboard = [];
 
     items.forEach((i) => {
       keyboard.push({
+        // @ts-ignore
         text: `${kbtype.includes('group') ? i.split(' ')[0].replace(',', '') : i}`,
         callback_data: `${kbtype}-${id}-${i}`,
       });
     });
 
+    // @ts-ignore
     return keyboard.chunk(chunk);
   }
+
+  showRemoteWorks() {}
 }
